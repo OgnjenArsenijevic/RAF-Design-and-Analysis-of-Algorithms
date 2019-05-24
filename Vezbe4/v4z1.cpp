5850fc7b501243ec6488c6a841d4980eb7be4570
OgnjenArsenijevic <30389716+OgnjenArsenijevic@users.noreply.github.com>
2019-05-24 19:27:26 +0200

    C++

--- Vezbe4/v4z1.cpp
+++ Vezbe4/v4z1.cpp
@@ -23,7 +23,7 @@ int main()
     dp[1]=1;
     for(int i=1;i<s.size();i++)
     {
-        if(s[i]=='0')
+        if(s[i]=='0' && s[i-1]!='1' && s[i-1]!='2')
         {
             cout<<0<<"\n";
             return 0;
